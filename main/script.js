613f2819522ca57fb459e2ce236c15e41369d73c
Evgeny Belov <belov.zhenya-belov1999@yandex.ru>
2021-01-12 17:40:35 +0300

    d

--- main/script.js
+++ main/script.js
@@ -1,64 +1,10 @@
 window.addEventListener("DOMContentLoaded", (e) => {
-  const track = slider__track;
-  const next = slider__next;
-  const pre = slider__pre;
-  let pos = track.style.left.split("p")[0];
-  track.style.left = 0;
-  let slideCount = -1;
-  const slides = document.querySelectorAll(".slide");
-  let slideWidth = 0;
-  slides.forEach((item) => {
-    var marginLeft = parseInt(getComputedStyle(item, true).marginLeft);
-    var marginRight = parseInt(getComputedStyle(item, true).marginRight);
-    slideWidth = item.offsetWidth + marginLeft + marginRight
-  });
-  console.log(slideWidth, slideCount);
+ 
 
-  next.addEventListener("click", () => {
-    if (pos <= -slideWidth * 4) {
-      pos = 0
-    } else {
-      pos -= slideWidth;
-    }
-    track.style.left = pos + "px";
-    console.log(track.style.left);
+  $('.slider').slick({
+    variableWidth: true,
   });
-  pre.addEventListener("click", () => {
-    if (pos >= 0) {
-      pos = -slideWidth * 4
-    } else {
-      pos += slideWidth;
-    }
-      track.style.left = pos + "px";
-  });
-  setInterval(()=>{
-    if (pos <= -slideWidth * 4) {
-      pos = 0
-    } else {
-      pos -= slideWidth;
-    }
-    track.style.left = pos + "px";
-  }, 5500)
-  Hammer(slider).on("swiperight", (e) => {
-    if (pos >= 0) {
-    } else {
-      pos += slideWidth;
-    }
-    if (pos > 0) {
-    } else {
-      track.style.left = pos + "px";
-    }
-  });
-  Hammer(slider).on("swipeleft", (e) => {
-    if (pos <= -slideWidth * 4) {
-    } else {
-      pos -= slideWidth;
-    }
-    if (pos < -slideWidth * 4) {
-    } else {
-      track.style.left = pos + "px";
-    }
-  });
+
   fullscreen.addEventListener("click", () => {
     console.log(video);
     console.log(video.requestFullscreen());
