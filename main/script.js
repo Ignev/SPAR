62058e685fe0c45d2851ec09a4b1750112c97ce9
Evgeny Belov <belov.zhenya-belov1999@yandex.ru>
2021-01-13 12:12:39 +0300

    d

--- main/script.js
+++ main/script.js
@@ -2,8 +2,18 @@ window.addEventListener("DOMContentLoaded", (e) => {
  
 
   $('.slider').slick({
-    variableWidth: true,
-    // autoplay: true,
+    infinite: true,
+		draggable: true,
+		slidesPerRow: 1,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		waitForAnimate: true,
+		swipeToSlide: true,
+		touchThreshold: 100,
+		speed: 500,
+		pauseOnHover: true,
+		autoplaySpeed: 3000,
+		arrows: true,
   });
 
   fullscreen.addEventListener("click", () => {
